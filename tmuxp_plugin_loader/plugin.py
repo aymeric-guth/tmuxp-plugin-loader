40c1106d8ca5c01806f1c4e6b617e35958269ce1
yul <aymeric.guth@protonmail.com>
2022-11-01 20:05:01 +0100

    clean+trace

--- tmuxp_plugin_loader/plugin.py
+++ tmuxp_plugin_loader/plugin.py
@@ -2,6 +2,8 @@ import os
 import libtmux
 from tmuxp.plugin import TmuxpPlugin
 import datetime
+import time
+import pdb
 
 Session = libtmux.session.Session
 Window = libtmux.window.Window
@@ -11,6 +13,24 @@ class Loader(TmuxpPlugin):
     def __init__(self):
         config = {"tmuxp_min_version": "1.6.2"}
         TmuxpPlugin.__init__(self, plugin_name="tmuxp-plugin-loader", **config)
+
+    def after_config_loaded(self, config):
+        # pdb.set_trace()
+        if not config.get("session_name") and os.getenv("PROJECT_NAME"):
+            config.update({"session_name": "$PROJECT_NAME"})
+            # config.update({"session_name": os.getenv("PROJECT_NAME")})
+        if not config.get("start_directory") and os.getenv("WORKSPACE"):
+            config.update({"start_directory": "$WORKSPACE"})
+            # config.update({"start_directory": os.getenv("WORKSPACE")})
+        if not config.get("shell_command_before"):
+            config.update(
+                {
+                    "shell_command_before": [
+                        "[ -f .func.sh ] && source .func.sh || return 0"
+                    ]
+                }
+            )
+        return config
 
     def before_workspace_builder(self, session: Session):
         project_name = os.getenv("PROJECT_NAME")
