5721b7adc581c0ecb8c430ac6b67abf7523deb16
yul <aymeric.guth@protonmail.com>
2022-11-01 20:49:19 +0100

    clean

--- tmuxp_plugin_loader/plugin.py
+++ tmuxp_plugin_loader/plugin.py
@@ -1,9 +1,6 @@
 import os
 import libtmux
 from tmuxp.plugin import TmuxpPlugin
-import datetime
-import time
-import pdb
 
 Session = libtmux.session.Session
 Window = libtmux.window.Window
@@ -33,27 +30,30 @@ class Loader(TmuxpPlugin):
         return config
 
     def before_workspace_builder(self, session: Session):
-        project_name = os.getenv("PROJECT_NAME")
-        if not project_name:
-            raise OSError("PROJECT_NAME is not defined")
-        workspace = os.getenv("WORKSPACE")
-        if not workspace:
-            raise OSError("WORKSPACE is not defined")
-        session.set_environment("WORKSPACE", workspace)
-        session.set_environment("PROJECT_NAME", project_name)
+        def raiser(varname):
+            var = os.getenv(varname)
+            if not var:
+                import pdb
+
+                pdb.set_trace()
+                raise OSError(f"{varname} is not defined")
+            return var
+
+        session.set_environment("WORKSPACE", raiser("WORKSPACE"))
+        session.set_environment("PROJECT_NAME", raiser("PROJECT_NAME"))
         # session._info
 
     def on_window_create(self, window: Window):
-        ...
         # window._info
+        ...
 
     def after_window_finished(self, window: Window):
         ...
 
     def before_script(self, session: Session):
-        "[ -f .func.sh ] && source .func.sh || return 0"
         ...
 
     def reattach(self, session: Session):
-        now = datetime.datetime.now().strftime("%Y-%m-%d")
-        session.rename_session("session_{}".format(now))
+        ...
+        # now = datetime.datetime.now().strftime("%Y-%m-%d")
+        # session.rename_session("session_{}".format(now))
