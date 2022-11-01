ae63c7ea6b1c28867cbf07ec1c8c0ab6820b3a8c
yul <aymeric.guth@protonmail.com>
2022-11-01 14:02:07 +0100

    bare minimum functional plugin

--- tmuxp_plugin_loader/plugin.py
+++ tmuxp_plugin_loader/plugin.py
@@ -1,26 +1,21 @@
 from tmuxp.plugin import TmuxpPlugin
 import datetime
 
-class MyTmuxpPlugin(TmuxpPlugin):
+
+class Loader(TmuxpPlugin):
     def __init__(self):
         """
         Initialize my custom plugin.
         """
         # Optional version dependency configuration. See Plugin API docs
         # for all supported config parameters
-        config = {
-            'tmuxp_min_version' = '1.6.2'
-        }
+        config = {"tmuxp_min_version": "1.6.2"}
 
-        TmuxpPlugin.__init__(
-            self,
-            plugin_name='tmuxp-plugin-my-tmuxp-plugin',
-            **config
-        )
+        TmuxpPlugin.__init__(self, plugin_name="tmuxp-plugin-loader", **config)
 
     def before_workspace_builder(self, session):
-        session.rename_session('my-new-session-name')
+        session.rename_session("testin-tests")
 
     def reattach(self, session):
-        now = datetime.datetime.now().strftime('%Y-%m-%d')
-        session.rename_session('session_{}'.format(now))
+        now = datetime.datetime.now().strftime("%Y-%m-%d")
+        session.rename_session("session_{}".format(now))
