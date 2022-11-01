ff452474903994accad3607dcf99eb0a4197ab1c
yul <aymeric.guth@protonmail.com>
2022-11-01 18:31:20 +0100

    migration d'une partie des features vers un patch sur tmuxp (config-loader)

--- tmuxp_plugin_loader/plugin.py
+++ tmuxp_plugin_loader/plugin.py
@@ -1,21 +1,39 @@
+import os
+import libtmux
 from tmuxp.plugin import TmuxpPlugin
 import datetime
+
+Session = libtmux.session.Session
+Window = libtmux.window.Window
 
 
 class Loader(TmuxpPlugin):
     def __init__(self):
-        """
-        Initialize my custom plugin.
-        """
-        # Optional version dependency configuration. See Plugin API docs
-        # for all supported config parameters
         config = {"tmuxp_min_version": "1.6.2"}
-
         TmuxpPlugin.__init__(self, plugin_name="tmuxp-plugin-loader", **config)
 
-    def before_workspace_builder(self, session):
-        session.rename_session("testin-tests")
+    def before_workspace_builder(self, session: Session):
+        project_name = os.getenv("PROJECT_NAME")
+        if not project_name:
+            raise OSError("PROJECT_NAME is not defined")
+        workspace = os.getenv("WORKSPACE")
+        if not workspace:
+            raise OSError("WORKSPACE is not defined")
+        session.set_environment("WORKSPACE", workspace)
+        session.set_environment("PROJECT_NAME", project_name)
+        # session._info
 
-    def reattach(self, session):
+    def on_window_create(self, window: Window):
+        ...
+        # window._info
+
+    def after_window_finished(self, window: Window):
+        ...
+
+    def before_script(self, session: Session):
+        "[ -f .func.sh ] && source .func.sh || return 0"
+        ...
+
+    def reattach(self, session: Session):
         now = datetime.datetime.now().strftime("%Y-%m-%d")
         session.rename_session("session_{}".format(now))
